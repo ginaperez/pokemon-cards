b5be877c676a179ef6086c44262feb2f154f3b9e
ginaperez <gmperez92@gmail.com>
2019-10-03 11:26:03 -0700

    gina perez morning changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://api.pokemontcg.io/v1/cards
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      cardCollection: [
+        {
+          name: "Cubone",
+          imageUrl: "https://images.pokemontcg.io/dp6/90.png"
+        },
+        {
+          name: "Windstorm",
+          imageUrl: "https://images.pokemontcg.io/ex14/85.png"
+        },
+        {
+          name: "Turtwig",
+          imageUrl: "https://images.pokemontcg.io/pop9/17.png"
+        }
+      ]
+    };
+    }
+  
+    componentDidMount() {
+      axios.get("https://api.pokemontcg.io/v1/cards").then((response) => {
+        this.setState({
+          cardCollection: response.data.cards
+        })
+      })
+    }
+  
+    render() {
+    const {cardCollection} = this.state;
+    const mappedCards = cardCollection.map(card => {
+      return (
+      <div key={card.imageUrl} className="poke-card">
+        <img src={card.imageUrl} />
+        <div>{card.name}</div>
+      </div>
+      );
+    });
+    return (
+      <div className="App">{mappedCards}</div>
+    );
+  }
 }
 
 export default App;
